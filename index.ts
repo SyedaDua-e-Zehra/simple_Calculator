a65d1e6651617b0b6ac0fb8923bf2c57414b7b3b
Syeda Dua e Zehra <syedaduaezehrar@gmail.com>
2024-03-21 06:02:21 +0500

    Push my calculator to GitHub

---
+++ index.ts
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+//Asking question from users through inquirer
+let answers = await inquirer.prompt([
+  {message:"Enter First Number",type:"number",name:"firstNumber"},
+  {message:"Enter Second Number",type:"number",name:"secondNumber"},
+  {
+
+   message: "Select one of the following operations:",
+   type:"list",
+   name:"operator",
+   choices:['Addition','Subtraction','Multiplication','Division'], 
+  }  
+]);
+// console.log(answers);
+//conditional Statements If Else
+if(answers.operator === "Addition"){
+    console.log(answers.firstNumber + answers.secondNumber);
+}
+else if(answers.operator === "Subtraction"){
+    console.log(answers.firstNumber - answers.secondNumber);
+}
+else if(answers.operator === "Multiplication"){
+    console.log(answers.firstNumber * answers.secondNumber);
+}
+else if(answers.operator === "Division"){
+    console.log(answers.firstNumber / answers.secondNumber);
+}
+else ("Invalid Input");
